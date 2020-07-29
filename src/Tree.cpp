76f05a7aeaca65228785e52e0b49cd2719b1ebe5
TodRieter <38633822+TodRieter@users.noreply.github.com>
2020-07-29 15:29:54 -0500

    Tree stuff

--- src/Tree.cpp
+++ src/Tree.cpp
@@ -10,7 +10,7 @@
 #include "Node.h"
 using namespace std;
 int main() {
-	cout << "I am Alive!!" << endl; // prints "I am Alive!!"
+	cout << "I am Alive!!!" << endl; // prints "I am Alive!!"
 	//Node root = *Node::build_tree_(7);
 //	cout << root.id_ << endl;
 //	cout << root.Child_L_->id_ << endl;
